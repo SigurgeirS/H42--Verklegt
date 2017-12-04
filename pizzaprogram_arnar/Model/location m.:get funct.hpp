d2dce02fd943033edac80248e1317aac8d5943d1
Alvarlegur <34064054+Alvarlegur@users.noreply.github.com>
2017-12-04 13:50:24 +0000

    Add files via upload
    location með get function

---
+++ pizzaprogram_arnar/Model/location m.:get funct.hpp
@@ -0,0 +1,22 @@
+#ifndef LOCATION_H
+#define LOCATION_H
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class location {
+private:
+	int ID;
+	string name;
+	string address;
+public:
+	location();
+	location(int id, int n, int addr);
+	get_location();
+	
+	friend istream& operator >> (istream& in, location& loc);
+	friend ostream& operator << (ostream& out, location &loc);
+};
+
+#endif
