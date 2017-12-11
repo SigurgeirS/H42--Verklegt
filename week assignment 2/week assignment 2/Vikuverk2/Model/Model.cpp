56af9bb1f2840460741cfa2ca763d566fb9cab74
Sigurgeir <sigurgeir16@ru.is>
2017-12-11 09:28:04 +0000

    copy paste...

--- week assignment 2/week assignment 2/Vikuverk2/Model/Model.cpp
+++ week assignment 2/week assignment 2/Vikuverk2/Model/Model.cpp
@@ -7,17 +7,9 @@ Model::Model()
     this->month = "";
     this->year = "";
 }
-string Model::get_name(){
-	return this->name;
-}
-
 string Model::get_ssn()
 {
     return this->ssn;
-}
-
-string Model::get_salary(){
-	return this->salary;
 }
 
 string Model::get_month()
@@ -30,50 +22,88 @@ string Model::get_year()
     return this->year;
 }
 
-istream& operator >> (istream& in, Model& model)
-{
-    cout << "Enter name: ";
-    in >> model.name;
-    cout << "Enter social security number: ";
-    in >> model.ssn;
-    cout << "Enter salary: ";
-    in >> model.salary;
-    cout << "Enter month: ";
-    in >> model.month;
-    cout << "Enter year: ";
-    in >> model.year;
-
+void Model::set_ssn(string ssn){
     try{
-	    Validate val;
-	    val.okName(model.getName());
-	    val.okSSN(model.get_ssn());
-	    val.okYear(model.get_year());
-	    val.okSalary(model.get_salary());
-	    val.okMonth(model.get_month());
+    Validate validate;
+    validate.okSSN(ssn);
+    this->ssn = ssn;
     }
-    catch(InvalidNameException){
-	    cout<<"Invalid input in Name"<<endl;
+     catch(InvalidSsnException){
+        cout << "Invalid ssn!" << endl;
     }
-    catch(InvalidSsnException){
-	    cout<<"Invalid input in SSN"<<endl;
+}
+
+void Model::set_name(string name){
+    try{
+        Validate validate;
+        validate.okName(name);
+        this->name = name;
     }
-    catch(InvalidYearException){
-	    cout<<"Invalid input in Year"<<endl;
+      catch(InvalidNameException){
+        cout << "Invalid name!" << endl;
+    }
+}
+
+void Model::set_salary(string salary){
+    try{
+        Validate validate;
+        validate.okSalary(salary);
     }
     catch(InvalidSalaryException){
-	    cout<<"Invalid input in Salary"<<endl
+        cout << "Invalid salary!" << endl;
+    }
+}
+
+void Model::set_year(string year){
+
+    try{
+    Validate validate;
+    validate.okYear(year);
+    this->year = year;
+    }
+     catch(InvalidYearException){
+        cout << "Invalid year!" << endl;
+    }
+}
+void Model::set_month(string month){
+    try{
+    Validate validate;
+    validate.okMonth(month);
+    this->month = month;
     }
     catch(InvalidMonthException){
-	    cout<<"Invalid input in Month"<<endl
+        cout << "Invalid month!" << endl;
     }
+}
 
-
+istream& operator >> (istream& in, Model& model)
+{
+    Validate validate;
+    cout << "Enter name: ";
+    in >> model.name;
+    model.set_name(model.name);
+    cout << "Enter social security number: ";
+    in >> model.ssn;
+    model.set_ssn(model.ssn);
+    cout << "Enter salary: ";
+    in >> model.salary;
+    model.set_salary(model.salary);
+    cout << "Enter month: ";
+    in >> model.month;
+    model.set_month(model.month);
+    cout << "Enter year: ";
+    in >> model.year;
+    model.set_year(model.year);
     return in;
 }
 
 ostream& operator << (ostream& out, Model& model)
 {
-    out << model.name << "," << model.ssn << "," << model.salary << "," << model.month << "," << model.year << endl;
+    stringstream stream;
+    stream << model.name;
+    string name = "";
+    getline(stream,name);
+    out << name << "," << model.ssn << "," << model.salary << "," << model.month << "," << model.year << endl;
     return out;
 }
 
