c0d4ac3e6401aa6e684fdcd920aba5a49a0d0f6e
Arnar Freyr Kristinsson <arnar11@hotmail.com>
2017-12-12 15:20:17 +0000

    something

--- pizzaprogram_arnar/main.cpp
+++ pizzaprogram_arnar/main.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <iostream>
 #include ".\UI\MainUI.h"
 using namespace std;
@@ -8,3 +9,15 @@ int main()
     mainui.startUI();
     return 0;
 }
+=======
+#include <iostream>
+#include ".\UI\MainUI.h"
+using namespace std;
+
+int main()
+{
+    MainUI mainui;
+    mainui.startUI();
+    return 0;
+}
+>>>>>>> a8bf7059d9df22810c8fa6bd54be9ab501350072
