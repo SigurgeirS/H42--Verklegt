d02678fc97bba8bd00969efbb98c166ba3052f4f
Alvarlegur <34064054+Alvarlegur@users.noreply.github.com>
2017-12-12 15:37:50 +0000

    Add files via upload

---
+++ pizzaprogram_arnar/Domain/Deliverydomain.cpp
@@ -0,0 +1,31 @@
+//
+//  domain.cpp
+//  Verklegt namskeið
+//
+//  Created by Halldór Alvar Kjartansson on 12/12/2017.
+//  Copyright © 2017 Halldór Alvar Kjartansson. All rights reserved.
+//
+
+#include "domain.hpp"
+#include "..\location m./get funct.hpp"
+
+char location::get_location(char input){
+//	Notandi velur sinn afhendingarstað
+	
+}
+void Domain::print_pizzalist(){
+//	Fá upp lista af pöntunum, bara fyrir þennan stað
+}
+void Domain::print_ready_pizza(){
+//	Fá upp lista af tilbúnum pöntunum, bara fyrir þennan stað
+}
+void Domain::get_specific_order(){
+//	Fá upp tiltekna pöntun
+}
+bool Domain::paid(){
+//	Merkja pöntun greidda
+}
+bool Domain::delivered(){
+//	Merkja pöntun afhenta
+}
+
