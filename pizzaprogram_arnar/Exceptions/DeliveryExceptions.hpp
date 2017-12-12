99740b410bf1ddc48d4a07c45c7154da87c5a834
Alvarlegur <34064054+Alvarlegur@users.noreply.github.com>
2017-12-12 15:47:14 +0000

    Add files via upload

---
+++ pizzaprogram_arnar/Exceptions/DeliveryExceptions.hpp
@@ -0,0 +1,14 @@
+//
+//  Exceptions.hpp
+//  Verklegt namskeið
+//
+//  Created by Halldór Alvar Kjartansson on 12/12/2017.
+//  Copyright © 2017 Halldór Alvar Kjartansson. All rights reserved.
+//
+
+#ifndef Exceptions_hpp
+#define Exceptions_hpp
+
+#include <stdio.h>
+
+#endif /* Exceptions_hpp */
